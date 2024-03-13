dd834a5a9c4de3fc5b1b3c93666752852307c477
Capataina <ata02caner@gmail.com>
2024-03-13 21:44:26 +0000

    changes

--- environments/bspwm/home.nix
+++ environments/bspwm/home.nix
@@ -43,7 +43,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    ".config/alacritty/alacritty.toml".source = ./AllPackages/dotconfigs/alacritty.toml;
+    ".config/alacritty/alacritty.toml".source = ../.././AllPackages/dotconfigs/alacritty.toml;
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
