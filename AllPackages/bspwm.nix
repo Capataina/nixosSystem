77df580858b443c744415f73a648831fd0b3bf74
Capataina <ata02caner@gmail.com>
2024-03-17 00:24:33 +0000

    saving final changes

--- AllPackages/bspwm.nix
+++ AllPackages/bspwm.nix
@@ -5,7 +5,11 @@
   home.file = {
     ".config/bspwm/bspwmrc".source = ./dotconfigs/bspwm/bspwmrc;
     ".config/picom/picom.conf".source = ./dotconfigs/picom/picom.conf;
+
     ".config/polybar/config.ini".source = ./dotconfigs/polybar/config.ini;
+    ".config/polybar/launch.sh".source = ./dotconfigs/polybar/launch.sh;
+    ".config/Scripts/".source = ./dotconfigs/Scripts;
+
     ".config/sxhkd/sxhkdrc".source = ./dotconfigs/sxhkd/sxhkdrc;
   };
 
