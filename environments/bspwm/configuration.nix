a4fb416a4f3013c46b59e7788337f31df4882f84
Capataina <ata02caner@gmail.com>
2024-03-18 16:25:51 +0000

    working on polybar workspace icons

--- environments/bspwm/configuration.nix
+++ environments/bspwm/configuration.nix
@@ -113,16 +113,21 @@ in
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
+
+    picom
     bspwm
     sxhkd
+    feh
+    polybarFull
+
     alacritty
     firefox
-    polybarFull
     nerdfonts
-    picom
     starship
+
     gnome.nautilus
     xfce.thunar
+    ranger
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
