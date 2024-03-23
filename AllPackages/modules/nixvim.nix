4a5f26e5cbd720cbb9b65eae4b607535a7b4d038
Capataina <ata02caner@gmail.com>
2024-03-23 20:54:03 +0000

    default editor switched to nixvim, now configuring

--- AllPackages/modules/nixvim.nix
+++ AllPackages/modules/nixvim.nix
@@ -1,10 +1,14 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
 
+
+  home.packages = [ pkgs.nixfmt pkgs.nixpkgs-fmt ];
+
   programs.nixvim = {
-    enable = true;
+          enable = true;
     defaultEditor = true;
+
     viAlias = true;
     vimAlias = true;
     colorschemes.catppuccin = {
@@ -12,6 +16,20 @@
       flavour = "mocha";
     };
 
+    plugins = {
+      lualine.enable = true;
+    };
+
+    plugins.lsp = {
+      enable = true;
+    };
+
+    keymaps = [{
+      action = "<Esc>:w<CR>";
+      key = "<C-s>";
+      mode = [ "n" "i" ];
+      options = { desc = "Save File"; };
+    }];
   };
 
 }
